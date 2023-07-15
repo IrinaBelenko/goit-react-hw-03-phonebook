6500479226f91473d23ea05e7f25dad88f3b49f0
IrinaBelenko <118209690+IrinaBelenko@users.noreply.github.com>
2023-07-15 18:03:23 +0300

    copy homework 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,78 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+import { nanoid } from 'nanoid';
+import { Filter } from './Filter/Filter';
+
+export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      contacts: [],
+      filter: '',
+      currentContacts: [],
+    };
+
+    this.state.currentContacts = this.state.contacts;
+  }
+
+  addContact = ({ name, number }) => {
+    const checkName = this.state.contacts.find(
+      contact => contact.name.toLowerCase() === name.toLowerCase()
+    );
+
+    if (checkName) {
+      alert(`${name} is already in contacts.`);
+      return;
+    }
+
+    const contact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+
+    this.setState(prev => {
+      const newContacts = [...prev.contacts, contact];
+      return {
+        contacts: newContacts,
+        currentContacts: newContacts,
+      };
+    });
+  };
+
+  filterContact = search => {
+    if (search === '') {
+      this.setState({ currentContacts: this.state.contacts });
+      return;
+    }
+
+    this.setState({
+      currentContacts: this.state.contacts.filter(contact =>
+        contact.name.toLowerCase().includes(search)
+      ),
+    });
+  };
+
+  deleteContact = id => {
+    this.setState(prev => {
+      const newContacts = prev.contacts.filter(contact => contact.id !== id);
+      return { contacts: newContacts, currentContacts: newContacts };
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <ContactForm addContact={this.addContact}> </ContactForm>
+        <h2>Contacts</h2>
+        <Filter filterContact={this.filterContact}> </Filter>
+        <ContactList
+          contacts={this.state.currentContacts}
+          deleteContact={this.deleteContact}
+        ></ContactList>
+      </div>
+    );
+  }
+}
